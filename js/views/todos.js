b54db9313d619780c78113b90de107d070381572
Evan Turner <evturn@gmail.com>
2014-12-31 18:40:47 -0500

    initialize and render function added to TodoView

--- js/views/todos.js
+++ js/views/todos.js
@@ -1,6 +1,22 @@
 var app = app || {};
 
 app.TodoView =  Backbone.View.extend({
+	tagName: 'li',
+	template: _.template($('#item-template').html()),
+	events: {
+    'dblclick label': 'edit',
+    'keypress .edit': 'updateOnEnter',
+    'blur .edit': 'close'
+  },
+  initialize: function() {
+    this.listenTo(this.model, 'change', this.render);
+  },
 
-	
+  // Rerenders the titles of the item
+  render: function() {
+    this.$el.html( this.template( this.model.attributes ) );
+    this.$input = this.$('.edit');
+    return this;
+  },
+
 });
