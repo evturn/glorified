a806b3cecc22805939395befa529d188664ae005
Evan Turner <evturn@gmail.com>
2014-12-31 17:27:52 -0500

    todo collection finished

--- js/collections/todos.js
+++ js/collections/todos.js
@@ -8,6 +8,17 @@ var TodoList = Backbone.Collection.extend({
 			return todo.get('completed');
 		});
 	},
-	
+	remaining: function() {
+		return this.without.applu(this, this.completed() );
+	},
+	nextOrder: function() {
+		if (!this.length) {
+			return 1;
+		}
+		return this.last().get('order') + 1;
+	},
+	comparator: function(todo) {
+		return todo.get('order');
+	}
 
 });
