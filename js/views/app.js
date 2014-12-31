aef2ea788f8d0a7d9a50f26847af7d1933ed0a65
Evan Turner <evturn@gmail.com>
2014-12-31 17:52:26 -0500

    initialize function added to appview

--- js/views/app.js
+++ js/views/app.js
@@ -1,7 +1,16 @@
 var app = app || {};
 
 app.AppView = Backbone.View.extend({
+	el: '#todoapp',
+	statsTemplate: _.template($('#stats-template').html()),
+	initialize: function() {
+    this.allCheckbox = this.$('#toggle-all')[0];
+    this.$input = this.$('#new-todo');
+    this.$footer = this.$('#footer');
+    this.$main = this.$('#main');
+    this.listenTo(app.Todos, 'add', this.addOne);
+    this.listenTo(app.Todos, 'reset', this.addAll);
+  },
 
 
-	
 });
