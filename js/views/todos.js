682c98ef4fd3a8b442cd4ee7b84bbe96361a256c
Evan Turner <evturn@gmail.com>
2014-12-31 19:33:41 -0500

    router created

--- js/views/todos.js
+++ js/views/todos.js
@@ -4,7 +4,9 @@ app.TodoView =  Backbone.View.extend({
 	tagName: 'li',
 	template: _.template($('#item-template').html()),
 	events: {
+		'click .toggle': 'togglecompleted',
     'dblclick label': 'edit',
+    'click .destroy': 'clear',
     'keypress .edit': 'updateOnEnter',
     'blur .edit': 'close'
   },
@@ -12,13 +14,30 @@ app.TodoView =  Backbone.View.extend({
   // If an indivdual item gets updated it will rerender the view
   initialize: function() {
     this.listenTo(this.model, 'change', this.render);
+    this.listenTo(this.model, 'destroy', this.remove);
+    this.listenTo(this.model, 'visible', this.toggleVisible);
   },
 
   // Rerenders the titles of the item
   render: function() {
     this.$el.html( this.template( this.model.attributes ) );
+    this.$el.toggleClass( 'completed', this.model.get('completed') );
+    this.toggleVisible();
     this.$input = this.$('.edit');
     return this;
+  },
+  toggleVisible : function () {
+    this.$el.toggleClass( 'hidden',  this.isHidden());
+  },
+  isHidden : function () {
+    var isCompleted = this.model.get('completed');
+    return ( // hidden cases only
+      (!isCompleted && app.TodoFilter === 'completed')
+      || (isCompleted && app.TodoFilter === 'active')
+    );
+  },
+  togglecompleted: function() {
+    this.model.toggle();
   },
 	edit: function() {
     this.$el.addClass('editing');
@@ -29,12 +48,18 @@ app.TodoView =  Backbone.View.extend({
     var value = this.$input.val().trim();
     if ( value ) {
       this.model.save({ title: value });
+    } else {
+    	this.clear();
     }
+
     this.$el.removeClass('editing');
   },
   updateOnEnter: function( e ) {
 	  if ( e.which === ENTER_KEY ) {
 	    this.close();
 	  }
-	}
+	},
+	clear: function() {
+    this.model.destroy();
+  }
 });
