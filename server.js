555ba17745cf3d9e1250babc5ac7e53e24467b70
Evan Turner <evturn@gmail.com>
2015-06-27 18:27:09 -0400

    config for express added

--- server.js
+++ server.js
@@ -8,5 +8,20 @@ var express         = require('express'),
     cookieParser    = require('cookie-parser'),
     mongoose        = require('mongoose'),
     passport        = require('passport'),
-    LocalStrategy   = require('passport-local').Strategy,
-    LocalConfig     = require('./config/passport-local')(passport);
+    hbs = require('./config/handlebars'),
+    root = __dirname + '/public';
+
+var app = module.exports = express();
+
+app.set('view engine', 'hbs');
+app.set('views', 'views');
+app.engine('hbs', hbs.engine);
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(root));
+app.use(logger('dev'));
+app.use(passport.initialize());
+app.use(passport.session());
+
+var http = require('./config/http');
