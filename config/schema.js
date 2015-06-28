f4e67855c12029ff13e662219d9a45ae9e5cc5c0
Evan Turner <evturn@gmail.com>
2015-06-28 04:54:03 -0400

    added schemas to exports object

--- config/schema.js
+++ config/schema.js
@@ -1,29 +1,35 @@
 var mongoose = require('mongoose');
 
-var User = mongoose.Schema({
-  local: {
-    username : {type : String, unique: true},
-    email    : {type : String, unique: true},
-    password : {type : String}
-  },
-  facebook: {
-    id       : {type : String},
-    token    : {type : String},
-    email    : {type : String},
-    name     : {type : String}
-  },
-  twitter: {
-    id       : {type : String},
-    token    : {type : String},
-    email    : {type : String},
-    name     : {type : String}
-  },
-  notes: [Note]
-});
+exports.User = function() {
+  return new mongoose.Schema({
+    local: {
+      username : {type : String, unique: true},
+      email    : {type : String, unique: true},
+      password : {type : String}
+    },
+    facebook: {
+      id       : {type : String},
+      token    : {type : String},
+      email    : {type : String},
+      name     : {type : String}
+    },
+    twitter: {
+      id       : {type : String},
+      token    : {type : String},
+      email    : {type : String},
+      name     : {type : String}
+    },
+    notes: [noteSchema]
+  });
+};
 
-var Note = mongoose.Schema({
-  list       : {type : String},
-  position   : {type : number},
-  created    : {type : Date, default: Date.now},
-  body       : {type : String}
-});
+exports.Note = function() {
+  return new mongoose.Schema({
+    list       : {type : String},
+    position   : {type : number},
+    created    : {type : Date, default: Date.now},
+    body       : {type : String}
+  });
+};
+
+var noteSchema = this.Note();
