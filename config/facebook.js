8c9b530f067f5604bfc5c4b86956062368019838
Evan Turner <evturn@gmail.com>
2015-06-28 04:10:25 -0400

    created config for db

---
+++ config/facebook.js
@@ -0,0 +1,7 @@
+module.exports = {
+  facebook: {
+    clientID: process.env.FACEBOOK_ID,
+    clientSecret: process.env.FACEBOOK_SECRET,
+    callbackURL: 'http://localhost:3000/auth/facebook/callback'
+  }
+};
---
+++ config/mongo.js
@@ -0,0 +1,7 @@
+var dbName = 'ramen-buffet';
+
+module.exports = {
+
+  url: 'mongodb://localhost:27017/' + dbName
+  
+};
