21c935cc16fbe894d236c0001f003bef1b322260
Evan Turner <evturn@gmail.com>
2015-06-28 05:31:26 -0400

    added monogodb config

--- config/mongo.js
+++ config/mongo.js
@@ -1,7 +1,21 @@
-var dbName = 'ramen-buffet';
+var mongodb       = require('mongodb');
+var mongoose      = require('mongoose');
+var dbName        = 'ramen-buffet';
 
-module.exports = {
-
-  url: 'mongodb://localhost:27017/' + dbName
+module.exports = function(moongoose){
   
+  mongoose.connect('mongodb://localhost/' + dbName, function(err) {
+      if (err) {
+        console.log('Ensure you\'re connected');
+      }
+    });
+
+  var db = mongoose.connection;
+
+  db.on('error', console.error.bind(console, 'connection error:'));
+
+  db.once('open', function callback() {
+    console.log('Connected to DB');
+  });
+
 };
