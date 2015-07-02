f9ee168b8274189b7a775d47850c2bc329995b19
Evan Turner <evturn@gmail.com>
2015-07-02 12:01:29 -0400

    created ajax call when creating note

--- assets/js/main.js
+++ assets/js/main.js
@@ -7,3 +7,40 @@ function colorGenerator() {
 $('.landing-header').on('click', function() {
   $('.landing-header a').css('color', colorGenerator());
 });
+
+
+$(function() {
+
+	$(document).on('click', '.new-note-btn', function(e) {
+		e.preventDefault();
+		createNote();
+	});
+
+});
+
+function createNote() {
+	var body = $('.new-note-input').val();
+	var category = $('.new-category-input').val();
+	$('.kurt-loader').html('<img src="img/dog.gif">');
+	console.log(location);
+	$.ajax({
+		url: '/notes',
+		type: 'POST',
+		dataType: 'json',
+		data: {
+			body: body,
+			category: category
+		},
+		success: function(data) {
+			console.log(data);
+			$('.kurt-loader').empty();
+			$('.kurt-loader').append('<p>We got ', data);
+			$('.new-note-input').val('');
+			$('.new-category-input').val('');
+		},
+		error: function(err) {
+			$('.kurt-loader').empty();
+			$('.kurt-loader').append('<p>We got ', error);
+		},
+	});
+}
