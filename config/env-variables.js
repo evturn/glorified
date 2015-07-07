be5e752f57014d7945c26ba5376bb3074db827d6
Evan Turner <evturn@gmail.com>
2015-07-06 22:51:34 -0400

    set callbackURL to production

--- config/env-variables.js
+++ config/env-variables.js
@@ -2,7 +2,7 @@ module.exports = {
   facebook : {
     clientID: process.env.FACEBOOK_ID,
     clientSecret: process.env.FACEBOOK_SECRET,
-    callbackURL: 'http://localhost:3000/auth/facebook/callback'
-    // callbackURL: 'http://ramenbuffet.com/auth/facebook/callback'
+    // callbackURL: 'http://localhost:3000/auth/facebook/callback'
+    callbackURL: 'http://ramenbuffet.com/auth/facebook/callback'
   }
 };
