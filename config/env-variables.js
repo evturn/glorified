98df4ba1dcac0d1d9edee6e8e30ace2b23377953
Evan Turner <evturn@gmail.com>
2015-08-31 15:30:04 -0700

    created keys for Twitter auth

--- config/env-variables.js
+++ config/env-variables.js
@@ -1,15 +1,17 @@
-var url;
-if (process.env.NODE_ENV === "development") {
-  url = 'http://localhost:3000/auth/facebook/callback';
-}
-else {
-  url = 'http://ramenbuffet.com/auth/facebook/callback';
-}
+var domain = process.env.NODE_ENV === "development" ? 'localhost:3000' : 'ramenbuffet.com';
 
 module.exports = {
+
   facebook : {
-    clientID: process.env.FACEBOOK_ID,
-    clientSecret: process.env.FACEBOOK_SECRET,
-    callbackURL: url
-  }
+    clientID         : process.env.FACEBOOK_ID,
+    clientSecret     : process.env.FACEBOOK_SECRET,
+    callbackURL      : 'http://' + domain + '/auth/facebook/callback'
+  },
+
+  'twitter' : {
+    'consumerKey'    : process.env.TWITTER_KEY,
+    'consumerSecret' : process.env.TWITTER_SECRET,
+    'callbackURL'    : 'http://' + domain + '/auth/twitter/callback'
+  },
+
 };
--- config/schema.js
+++ config/schema.js
@@ -3,13 +3,13 @@ var mongoose = require('mongoose');
 exports.User = function() {
   return new mongoose.Schema({
     username   : {type : String, sparse: true},
+    password   : {type : String, sparse: true},
     email      : {type : String, sparse: true},
     name       : {type : String, sparse: true},
     lastName   : {type : String, sparse: true},
     firstName  : {type : String, sparse: true},
     gender     : {type : String, sparse: true},
     fbProfile  : {type : String, sparse: true},
-    password   : {type : String, sparse: true},
     fbId       : {type : String},
     fbToken    : {type : String},
     fbEmail    : {type : String, sparse: true},
