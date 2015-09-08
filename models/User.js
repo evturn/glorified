800c86d2599a17ca8e36bc11e455fcb110fe88b4
Evan Turner <evturn@gmail.com>
2015-09-08 07:00:08 -0400

    refactored indentation in User model

--- models/User.js
+++ models/User.js
@@ -1,53 +1,7 @@
-var express = require('express');
-var passport = require('passport');
-var LocalStrategy = require('passport-local').Strategy;
-var mongoose = require('mongoose'),
-    bcrypt   = require('bcrypt');
-
-var passportLocalMongoose = require('passport-local-mongoose');
-
-var Note = new mongoose.Schema({
-    list         : {type : String},
-    created      : {type : Date,    default: new Date()},
-    body         : {type : String},
-    done         : {type : Boolean, default: false},
-    timestamp    : {type : String},
-    updated      : {type : String}
-});
-
-
-var List = new mongoose.Schema({
-    name         : {type : String},
-    icon         : {type : String,  default: 'fa fa-tasks'},
-    created      : {type : Date,    default: new Date()},
-    notes        : [Note],
-});
-
-var User = new mongoose.Schema({
-    email        : {type : String, sparse: true, unique: true},
-    password     : {type : String, sparse: true, required: true},
-    username     : {type : String, sparse: true, required: true, unique: true},
-    name         : {type : String, sparse: true},
-    facebook     : {
-      name       : {type : String, sparse: true},
-      lastName   : {type : String, sparse: true},
-      firstName  : {type : String, sparse: true},
-      gender     : {type : String, sparse: true},
-      profile    : {type : String, sparse: true},
-      id         : {type : String},
-      token      : {type : String},
-      email      : {type : String, sparse: true}
-    },
-    twitter      : {
-      id         : {type : String},
-      token      : {type : String},
-      username   : {type : String, sparse: true},
-      name       : {type : String, sparse: true},
-      location   : {type : String, sparse: true},
-      avatar     : {type : String, sparse: true}
-    },
-    lists        : [List]
-});
+var User = require('./schema').User(),
+    mongoose = require('mongoose'),
+    bcrypt = require('bcrypt'),
+    passportLocalMongoose = require('passport-local-mongoose');
 
 User.pre('save', function(next) {
   var user = this;
@@ -56,31 +10,35 @@ User.pre('save', function(next) {
     return next();
   }
 
-  bcrypt.genSalt(10, function(err, salt) {
-    if (err) {
-      return next(err);
-    }
-    bcrypt.hash(user.password, salt, function(err, hash) {
+  bcrypt.genSalt(10,
+    function(err, salt) {
       if (err) {
         return next(err);
       }
-      user.password = hash;
-      next(user);
+
+      bcrypt.hash(user.password, salt,
+        function(err, hash) {
+          if (err) {
+            return next(err);
+          }
+
+          user.password = hash;
+          next(user);
+      });
     });
-  });
 });
 
 User.methods.comparePassword = function(candidatePassword, cb) {
-  bcrypt.compare(candidatePassword, this.password, function(err, isMatch) {
-    if (err) {
-      return cb(err);
-    }
-    cb(null, isMatch);
+  bcrypt.compare(candidatePassword, this.password,
+    function(err, isMatch) {
+      if (err) {
+        return cb(err);
+      }
+
+      cb(null, isMatch);
   });
 };
 
-
 User.plugin(passportLocalMongoose);
-
 
 module.exports = mongoose.model('User', User);
---
+++ models/schema.js
@@ -0,0 +1,52 @@
+var mongoose = require('mongoose');
+
+var schema = this;
+
+exports.Note = function() {
+  return new mongoose.Schema({
+    list         : {type : String},
+    created      : {type : Date,    default: new Date()},
+    body         : {type : String},
+    done         : {type : Boolean, default: false},
+    timestamp    : {type : String},
+    updated      : {type : String}
+  });
+};
+
+
+exports.List = function() {
+  return new mongoose.Schema({
+    name         : {type : String},
+    icon         : {type : String,  default: 'fa fa-tasks'},
+    created      : {type : Date,    default: new Date()},
+    notes        : [schema.Note()],
+  });
+};
+
+exports.User = function() {
+  return User = new mongoose.Schema({
+    email        : {type : String, sparse: true, unique: true},
+    password     : {type : String, sparse: true, required: true},
+    username     : {type : String, sparse: true, required: true, unique: true},
+    name         : {type : String, sparse: true},
+    facebook     : {
+      name       : {type : String, sparse: true},
+      lastName   : {type : String, sparse: true},
+      firstName  : {type : String, sparse: true},
+      gender     : {type : String, sparse: true},
+      profile    : {type : String, sparse: true},
+      id         : {type : String},
+      token      : {type : String},
+      email      : {type : String, sparse: true}
+    },
+    twitter      : {
+      id         : {type : String},
+      token      : {type : String},
+      username   : {type : String, sparse: true},
+      name       : {type : String, sparse: true},
+      location   : {type : String, sparse: true},
+      avatar     : {type : String, sparse: true}
+    },
+    lists        : [schema.List()]
+  });
+};
